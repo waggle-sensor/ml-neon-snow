79994a6a8e1c7279f077152dfd6ed1b556dfc8e0
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-01-22 14:24:37 -0600

    check args

--- build-labelbox-csv-from-list.sh
+++ build-labelbox-csv-from-list.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "usage: $0 url-list-file"
+    exit 1
+fi
+
 echo "imageUrl,externalId"
 while read url < "$1"; do
     echo "$url,$(basename $url)"
--- download-image-list.sh
+++ download-image-list.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "usage: $0 url-list-file"
+    exit 1
+fi
+
 wget -r -N -i "$1"
